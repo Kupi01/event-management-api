ee6d2d618ebbad4ac23cb300674241112e52304c
Krupa Patel <krupapatel@academic.rrc.ca>
2025-11-16 14:57:05 -0600

    feat: add data storage for events and categories

--- src/data/category.ts
+++ src/data/category.ts
@@ -1,38 +1,4 @@
-import { Category } from '../models/Category';
+import { Category } from '../api/v1/models/Category';
 
-let categories: Category[] = [];
-
-export const categoryData = {
-  getAll: (): Category[] => {
-    return categories;
-  },
-
-  getById: (id: string): Category | undefined => {
-    return categories.find(category => category.id === id);
-  },
-
-  create: (category: Category): Category => {
-    categories.push(category);
-    return category;
-  },
-
-  update: (id: string, updatedCategory: Partial<Category>): Category | undefined => {
-    const index = categories.findIndex(category => category.id === id);
-    if (index === -1) return undefined;
-    
-    categories[index] = { ...categories[index], ...updatedCategory, id };
-    return categories[index];
-  },
-
-  delete: (id: string): boolean => {
-    const index = categories.findIndex(category => category.id === id);
-    if (index === -1) return false;
-    
-    categories.splice(index, 1);
-    return true;
-  },
-
-  clear: (): void => {
-    categories = [];
-  }
-};
+// In-memory storage for categories
+export const categories: Category[] = [];
--- src/data/event.ts
+++ src/data/event.ts
@@ -1,38 +1,4 @@
-import { Event } from '../models/Event';
+import { Event } from '../api/v1/models/Event';
 
-let events: Event[] = [];
-
-export const eventData = {
-  getAll: (): Event[] => {
-    return events;
-  },
-
-  getById: (id: string): Event | undefined => {
-    return events.find(event => event.id === id);
-  },
-
-  create: (event: Event): Event => {
-    events.push(event);
-    return event;
-  },
-
-  update: (id: string, updatedEvent: Partial<Event>): Event | undefined => {
-    const index = events.findIndex(event => event.id === id);
-    if (index === -1) return undefined;
-    
-    events[index] = { ...events[index], ...updatedEvent, id };
-    return events[index];
-  },
-
-  delete: (id: string): boolean => {
-    const index = events.findIndex(event => event.id === id);
-    if (index === -1) return false;
-    
-    events.splice(index, 1);
-    return true;
-  },
-
-  clear: (): void => {
-    events = [];
-  }
-};
+// In-memory storage for events
+export const events: Event[] = [];
